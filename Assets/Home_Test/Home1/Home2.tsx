0366af6821884c9935c2df05da2232c2fabcabb2
Unity-js <essaround@gmail.com>
2024-11-26 20:47:39 +0900

    [Add] Home_Test
    스테이지 5,6 에서 사용 할 TileMap 3종류 추가

---
+++ Assets/Home_Test/Home1/Home2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Home2" tilewidth="16" tileheight="16" tilecount="4968" columns="72">
+ <image source="../../Map_base/Bridge_1/CLT_Houses.png" width="1152" height="1104"/>
+</tileset>
