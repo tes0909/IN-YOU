0c6391a2f49a9223096f2ce28f36daa0445d6eae
tes0909 <krosa1213@gmail.com>
2024-11-28 12:20:33 +0900

    [edit] 중복생성 방지, 스크립트 분리

---
+++ Assets/Scripts/GridSpawn.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridSpawn : MonoBehaviour
+{
+    [SerializeField] private GameObject gridPrefab;
+    private HashSet<Vector3> spawnPositions = new HashSet<Vector3>();
+    private float xPosition;
+    private float nextYposition = 20f;
+    private BoxCollider2D b2d;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Vector3 spawnPostion = new Vector3(xPosition, transform.position.y + nextYposition, 0f);
+    
+            if (!spawnPositions.Contains(spawnPostion))
+            {
+                spawnPositions.Add(spawnPostion);
+                Instantiate(gridPrefab, spawnPostion, Quaternion.identity);
+            }
+        }
+    }
+}
