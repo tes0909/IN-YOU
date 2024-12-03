072ddd1fed6d5fe9780528a615eef4e438703288
Unity-js <essaround@gmail.com>
2024-12-03 20:15:23 +0900

    [Add] Bird
    Bird 몬스터 구성요소 추가 , 수정 필요

---
+++ Assets/Home_Test/Bird/Bird.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    public BirdStats birdStats;
+    public Transform player;
+
+    public BirdState idleState;
+    public BirdState chasingState;
+    public BirdState attackingState;
+
+    private BirdState currentState;
+
+    private void Start()
+    {
+        idleState = new BirdIdleState();
+        chasingState = new BirdChasingState();
+        attackingState = new BirdAttackingState();
+
+        idleState.SetBird(this);
+        chasingState.SetBird(this);
+        attackingState.SetBird(this);
+
+        currentState = idleState;
+    }
+
+    private void Update()
+    {
+        currentState.Update();
+    }
+
+    public void ChangeState(BirdState newState)
+    {
+        currentState.Exit();
+        currentState = newState;
+        currentState.Enter();
+    }
+}
---
+++ Assets/Home_Test/Bird/BirdAttackingState.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class BirdAttackingState : BirdState
+{
+    public override void Enter()
+    {
+        Debug.Log("공격"); // Test
+    }
+
+    public override void Update()
+    {
+        // 플레이어 공격 로직?
+        bird.ChangeState(bird.idleState); // 공격 > 대기
+    }
+
+    public override void Exit() { }
+}
