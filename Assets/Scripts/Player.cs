5cd1516878247e8bc946481963587c698b6d182d
tes0909 <krosa1213@gmail.com>
2024-12-04 15:57:09 +0900

    [update] 이동 기능 업데이트

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,28 +1,51 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [field: SerializeField] public PlayerSO PlayerSOData { get; private set; }
+    
     [field:Header("Animation")] 
     [field:SerializeField] public PlayerAnimationData AnimationData { get; private set; }
     
     public Animator Animator { get; private set; }
     public PlayerController input { get; private set; }
-    //public CharacterController controller { get; private set; } rigidbody?
+    public BoxCollider2D boxCollider2D { get; private set; }
+    public Rigidbody2D rigidbody2D { get; private set; }
+
+    private PlayerStateMachine stateMachine;
 
     void Awake()
     {
         AnimationData.Initialize();
         Animator = GetComponent<Animator>();
         input = GetComponent<PlayerController>();
+        boxCollider2D = GetComponent<BoxCollider2D>();
+        rigidbody2D = GetComponent<Rigidbody2D>();
+
+        stateMachine = new PlayerStateMachine(this);
+        
     }
 
     void Start()
     {
+        stateMachine.changeState(stateMachine.IdleState);
         Cursor.lockState = CursorLockMode.Locked;
     }
-    
+
+    private void Update()
+    {
+        stateMachine.Update();
+        stateMachine.HandleInput();
+    }
+
+    private void FixedUpdate()
+    {
+        stateMachine.PhysicsUpdate();
+    }
+
     public void RecoverHealth(int amount)
     {
         Debug.Log($"Health recovered by {amount}");
