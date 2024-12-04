0d8a37022345937b30c3990a30eea72b66b91196
tes0909 <krosa1213@gmail.com>
2024-12-04 09:58:20 +0900

    [rename] 스크립트명 수정

--- Assets/Scripts/Player/Playertest.cs
+++ Assets/Scripts/Player/Playertest.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Playertest : MonoBehaviour
 {
     public void RecoverHealth(int amount)
     {
