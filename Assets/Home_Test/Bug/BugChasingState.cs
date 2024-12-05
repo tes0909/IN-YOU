65fc27bf84982bf11b9ac86736655fab80bb9aef
Unity-js <essaround@gmail.com>
2024-12-05 20:18:19 +0900

    [Add] Bug

---
+++ Assets/Home_Test/Bug/BugChasingState.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class BugChasingState : BugState
+{
+    public override void Enter()
+    {
+        Debug.Log("추격 시작");
+    }
+
+    public override void Update()
+    {
+        Vector2 directionToPlayer = (bug.player.position - bug.transform.position).normalized;
+        bug.transform.Translate(directionToPlayer * bug.bugStats.moveSpeed * Time.deltaTime);
+
+        if (Vector2.Distance(bug.transform.position, bug.player.position) <= bug.bugStats.attackRange)
+        {
+            bug.ChangeState(bug.attackingState); // 범위 내 > 공격
+        }
+    }
+
+    public override void Exit() { }
+}
