9fb96745e1ba52f2c928e8abdd0e2b84670cfc71
tes0909 <krosa1213@gmail.com>
2024-12-20 10:23:30 +0900

    [update] 플레이어 체력 업데이트

--- Assets/Home_Test/Bug/BugAttackingState.cs
+++ Assets/Home_Test/Bug/BugAttackingState.cs
@@ -4,18 +4,25 @@ using UnityEngine;
 public class BugAttackingState : BugState
 {
     private BugStats bugStats;
+    private float attackCoolTime = 2.0f;
+    private float lastAttackTime;
     public override void Enter()
     {
         bugStats = bug.bugStats;
         Debug.Log("버그 공격");
+        lastAttackTime = 0;
     }
 
     public override void Update()
     {
-        // 실행안됨
-        PlayerAttackDamage();
-        Debug.Log("플레이어 공격중");
-        bug.ChangeState(bug.chasingState); 
+        lastAttackTime += Time.deltaTime; // 경과시간누적
+        if (lastAttackTime > attackCoolTime) // 쿨타임이 지났는지 확인
+        {
+            PlayerAttackDamage();
+            lastAttackTime = 0; // 마지막 공격 시간 업데이트
+            bug.ChangeState(bug.chasingState); 
+            Debug.Log("플레이어 공격중");
+        }
     }
 
     public override void Exit() { }
@@ -28,7 +35,15 @@ public class BugAttackingState : BugState
             PlayerCondition playerCondition = player.GetComponent<PlayerCondition>();
             if (playerCondition != null)
             {
-                playerCondition.TakeDamage(bugStats.attackPower);
+                float playerHealth = playerCondition.GetCurrentHealth();
+                if (playerHealth > 0)
+                {
+                    playerCondition.TakeDamage(bugStats.attackPower);
+                }
+                else
+                {
+                    Debug.Log("플레이어 사망");
+                }
             }
         }
     }
--- Assets/Resources/Prefabs/UI/PlayerCondition.cs
+++ Assets/Resources/Prefabs/UI/PlayerCondition.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,13 +10,15 @@ public class PlayerCondition : MonoBehaviour
     public PlayerStatData playerStatData;
     public BugStats bugStats;
     public Slider healthBar;
-    public float currentHealth;
-    public float maxHealth;
+    public TextMeshProUGUI healthText;
+    
+    private float currentHealth;
+    private float maxHealth;
 
     private void Start()
     {
         currentHealth = playerStatData.CurrentHealth;
-        maxHealth = playerStatData.MaxHealth;
+        maxHealth = playerStatData.MaxHealth; 
     }
 
     private void Update()
@@ -28,7 +31,13 @@ public class PlayerCondition : MonoBehaviour
         if (healthBar != null)
         {
             healthBar.value = currentHealth / maxHealth;
+            healthText.text = $"{currentHealth}/{maxHealth}";
         }
+    }
+
+    public float GetCurrentHealth()
+    {
+        return currentHealth;
     }
 
     public void TakeDamage(float damage)
@@ -36,7 +45,7 @@ public class PlayerCondition : MonoBehaviour
         currentHealth -= damage;
         Debug.Log($"플레이어가 {bugStats.bugName}에게 {damage}의 공격을 받았습니다. 현재 체력: {currentHealth}");
 
-        if (currentHealth <= 0)
+        if (currentHealth < 0)
         {
             Destroy(gameObject);
             Debug.Log("사망");
