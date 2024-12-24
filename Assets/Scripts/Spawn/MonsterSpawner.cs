9a57809f34e3b92d84df193392eaf41e35c0d9cb
Seo-Woongbi <suwoongby@naver.com>
2024-12-24 11:12:25 +0900

    [Foldering] 기존 몬스터 삭제

--- Assets/Scripts/Spawn/MonsterSpawner.cs
+++ Assets/Scripts/Spawn/MonsterSpawner.cs
@@ -4,5 +4,6 @@ using UnityEngine;
 
 public class MonsterSpawner : MonoBehaviour
 {
-    //몬스터 소환 플레이어 기준 원 방향으로 NevMash 사용해서 스폰이 가능한 곳에서만 스폰 될 수 있도록 할 것
+    //몬스터 소환 플레이어 기준 원 방향으로? - 아니면 맵의 스폰 포인트로 한번에 소환할 수 있도록? 
+    //NevMash 사용해서 스폰이 가능한 곳에서만 스폰 될 수 있도록 할 것
 }
