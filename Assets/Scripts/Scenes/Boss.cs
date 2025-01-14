88557535f0984582884e75950d4f41933468d088
tes0909 <krosa1213@gmail.com>
2025-01-14 10:18:35 +0900

    [edit] 씬 테스트, 수정

--- Assets/Scripts/Scenes/Boss.cs
+++ Assets/Scripts/Scenes/Boss.cs
@@ -7,9 +7,6 @@ public class Boss : SceneBase
     public DialogueUI dialogueUI;
     protected override void OnSceneLoad()
     {
-        Managers.UI.Init();
-        Managers.Sound.Init();
-        Managers.Game.Init();
         Invoke("SetStart", 1f);
         Managers.Sound.PlayBGM("10_Boss_Faintly");
     }
