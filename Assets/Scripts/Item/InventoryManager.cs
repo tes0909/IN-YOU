ad3fdf39f1dce444123ece5fc5c32f415c47f519
tes0909 <krosa1213@gmail.com>
2025-01-18 01:09:23 +0900

    [Edit] 개선점 반영(조작법 스타트화면 추가)

--- Assets/Scripts/Item/InventoryManager.cs
+++ Assets/Scripts/Item/InventoryManager.cs
@@ -9,6 +9,7 @@ public class InventoryManager : MonoBehaviour
     public InventoryUI inventoryUI;
     private bool isInventoryActive = false;
     public static InventoryManager instance;
+    public DialogueUI dialogueUI;
 
     private void Awake()
     {
@@ -24,15 +25,22 @@ public class InventoryManager : MonoBehaviour
     }
     private void Start()
     {
+        dialogueUI = FindObjectOfType<DialogueUI>();
+        if(dialogueUI == null) Debug.Log("Dialogue UI not found");
         inventoryUI.gameObject.SetActive(false);
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Tab))
+        if (!DialogueActive() && Input.GetKeyDown(KeyCode.Tab))
         {
             ToggleInventory();
         }
+    }
+
+    private bool DialogueActive()
+    {
+        return dialogueUI != null && dialogueUI.gameObject.activeSelf;
     }
     private void ToggleInventory()
     {
